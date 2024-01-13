19660c726dc48868115a839cde299aad77f1aecd
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-13 13:57:29 +0530

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -2,7 +2,7 @@ const corsOptions = {
     origin: ["http://localhost:5173", "https://password-reset-odmp.onrender.com", "*"],
     // origin: "https://password-reset-odmp.onrender.com",
     methods: ["GET", "POST"],
-    credentials: true
+    // credentials: true
 }
 
 module.exports = corsOptions;
