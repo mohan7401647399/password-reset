6a9fe5906e1d7ded2aa45011a9c7190aefb3ecc2
mohan7401647399 <mtm.kcs@gmail.com>
2024-01-13 08:06:02 +0530

    done

--- Frontend/src/Homepage.jsx
+++ Frontend/src/Homepage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 export default function Homepage() {
     return (
@@ -6,3 +7,13 @@ export default function Homepage() {
         </div>
     )
 }
+=======
+
+export default function Homepage() {
+    return (
+        <div>
+            <h1>Welcome to login page</h1>
+        </div>
+    )
+}
+>>>>>>> 8f2c60dcb042513b703c45d7daa60ca5590dbf1c
