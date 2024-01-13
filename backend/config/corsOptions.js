1aab1dc195c1c1f6e0f3e55da4c58edfb8959072
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-13 12:09:30 +0530

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,6 +1,7 @@
 const corsOptions = {
     //origin: ["http://localhost:5173","*"],
-    origin:["https://keen-melomakarona-8c650c.netlify.app","http://localhost:5173"]
+    //origin:["https://keen-melomakarona-8c650c.netlify.app","http://localhost:5173"]
+    origin: "https://password-reset-odmp.onrender.com",
     methods: ["GET", "POST"],
     credentials: true
 }
