84317e9513724ea5b08e697f34812d34681cc1e2
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-13 09:25:08 +0530

    Update Homepage.jsx

--- Frontend/src/Homepage.jsx
+++ Frontend/src/Homepage.jsx
@@ -1,7 +1,9 @@
 export default function Homepage() {
     return (
-        <div>
-            <h1>Welcome to login page</h1>
+        <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
+            <div className=''>                
+                    <h1>Welcome to login page</h1>                
+            </div>
         </div>
     )
 }
