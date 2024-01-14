64457f5c4f74662b053bb2da1b07dcc8b63385ee
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-14 09:51:53 +0530

    Update Homepage.jsx

--- Frontend/src/Homepage.jsx
+++ Frontend/src/Homepage.jsx
@@ -2,7 +2,7 @@ export default function Homepage() {
     return (
         <div className="container-fluid vh-100 bg-secondary">
             <div className="row justify-content-center">
-                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-12">
+                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
                     <div className="card mt-5 h-100 w-100">
                         <div className='bg-white p-3 rounded w-auto bg-info'>
                             <h1 className='d-flex align-content-center justify-content-center'>
