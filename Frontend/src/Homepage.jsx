e63f3adf7e35a5a167d65e1cf4ce9ea685fac652
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-14 09:42:35 +0530

    Update Homepage.jsx

--- Frontend/src/Homepage.jsx
+++ Frontend/src/Homepage.jsx
@@ -1,8 +1,16 @@
 export default function Homepage() {
     return (
-        <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
-            <div className=''>                
-                    <h1>Welcome to login page</h1>                
+        <div className="container-fluid vh-100 bg-secondary">
+            <div className="row justify-content-center">
+                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-12">
+                    <div className="card mt-5 h-100 w-100">
+                        <div className='bg-white p-3 rounded w-auto bg-info'>
+                            <h1 className='d-flex align-content-center justify-content-center'>
+                                Welcome to login page
+                            </h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
