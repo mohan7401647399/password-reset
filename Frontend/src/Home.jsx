0a148222526293040730b7db87c812aa41cfe9f6
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-14 09:41:21 +0530

    Update Home.jsx

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
-            <div className=''>
-                <h1>
-                    <Link to="/login">Login here</Link>
-
-                </h1>
+        <div className="container-fluid vh-100 bg-secondary">
+            <div className="row justify-content-center">
+                <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-12">
+                    <div className="card mt-5 h-100 w-100">
+                        <div className='bg-white p-3 rounded w-auto bg-info'>
+                            <h1 className='d-flex align-content-center justify-content-center'>
+                                <Link to="/login">Login here</Link>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
